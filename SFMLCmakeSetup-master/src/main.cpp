ffaff701fb883fad8246fd752bf07f1912e9b7e3
Fabio Mijango <00156022@uca.edu.sv>
2025-06-13 22:12:29 -0600

    Carga de mapa en base a Map.txt, y dibujado en pantalla

--- SFMLCmakeSetup-master/src/main.cpp
+++ SFMLCmakeSetup-master/src/main.cpp
@@ -1,76 +1,82 @@
-
-#include <SFML/Graphics.hpp>
+﻿#include <SFML/Graphics.hpp>
+#include "MapLoader.hpp"
+#include "Utils.hpp"
+#include "HexCell.hpp"
 #include <vector>
+#include <iostream>
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+
+#define HEXAGON_RADIUS 25
+#define HEXAGON_OUTLINE_THICKNESS 4
+
+#define MAP_PATH "resources/map.txt"
+#define FONT_PATH "resources/arial.ttf"
 
 int main()
 {
-    sf::Color colors[2] = { sf::Color::Red, sf::Color::Green };
-	sf::Color ballColors[2] = { sf::Color::Blue, sf::Color::Yellow };
-    int ballX = 640 / 2;
-	int bally = 480 / 2;
-
-    sf::RenderWindow window({ 640, 480 }, "Hexagons", sf::Style::Default, sf::ContextSettings(0, 0, 8));
-
-    sf::CircleShape hexagon(25, 6);
-    hexagon.setOutlineColor(sf::Color::Black);
-    hexagon.setOutlineThickness(5);
-    hexagon.setFillColor(sf::Color::White);
-    hexagon.setOrigin(25, 25);
-
-	sf::CircleShape circle(20);
-	circle.setFillColor(sf::Color::Blue);
+	// Inicializar las celdas del grid
+	std::vector<std::vector<HexCell>> grid = loadMapFromFile(MAP_PATH);
 
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	// Crear un objeto de tipo sf::CircleShape para representar un hexágono
+	sf::CircleShape hexagon(25, 6);
+	hexagon.setOutlineColor(sf::Color::Black);
+	hexagon.setFillColor(sf::Color::White);
+	hexagon.setOrigin(25, 25);
+	hexagon.setOutlineThickness(HEXAGON_OUTLINE_THICKNESS);
+	sf::Color color[2] = { sf::Color(220,220,220), sf::Color(192,192,192)};
+
+	// Crear una ventana de SFML
+	sf::RenderWindow window({ WINDOW_WIDTH, WINDOW_HEIGHT }, "Hexagons", sf::Style::Titlebar | sf::Style::Close, sf::ContextSettings(0, 0, 8));
+
+	sf::Font font;
+	if (!font.loadFromFile(FONT_PATH)) {
+		std::cerr << "No se pudo cargar la fuente.\n";
+	}
+	sf::Text texto = createText(font, 16, sf::Color::Black);
+
+	// Ciclo principal
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+
+		}
 
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-            if (ballX > 0) // 20 es el radio del c�rculo
-                ballX -= 1;
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-            if (ballX + 20 < 640)
-                ballX += 1;
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-            if (bally > 0)
-                circle.setFillColor(ballColors[bally % 2]);
-                bally -= 1;
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-            if (bally + 20 < 480)
-                circle.setFillColor(ballColors[bally % 2]);
-                bally += 1;
-        }
-        
+		// Logica del juego
 
 
 
-        window.clear(sf::Color::White);
+		// Desde aqui, logica del dibujo
+		window.clear(sf::Color::White);
 
 
+		// Dibujar el grid de hexágonos
+		for (int y = 0; y <= grid.size() - 1; ++y) {
+			for (int x = 0; x <= grid[y].size() - 1; ++x) {
+                
+				// Ajustar la posición del hexágono según las coordenadas x, y
+				hexagon.setPosition((y % 2 ? 75 : 50) + x * 50.f, 50 + y * 40.f);
+				hexagon.setFillColor(color[y % 2]);
+				window.draw(hexagon);
 
-        for (int y = 0; y < 10; ++y) {
-            for (int x = 0; x < 10; ++x) {
-                // The horizontal/vertical offsets of 50 and 40
-                // might feel off, but that's due to the way SFML
-                // calculates the circle outline
-                hexagon.setPosition((y % 2 ? 75 : 50) + x * 50.f, 50 + y * 40.f);
-                hexagon.setFillColor(colors[y % 2]);
-                window.draw(hexagon);
-            }
-        }
+				texto.setString(CellTypeToString(grid[y][x].type));
+				texto.setPosition((y % 2 ? 65 : 40) + x * 50.f, 45 + y * 40.f);
+				window.draw(texto);
+			}
+		}
 
-        //(y % 2 ? 75 : 50) + x * 50.f
-        circle.setPosition(ballX, bally);
+		window.display();
+		
+	}
 
-		window.draw(circle);
-
-        window.display();
-    }
 }
+
+
+
